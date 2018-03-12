5a2de6f48343dac9768cfe9a1fdc7a8893fb546d
Nitin Singh <singhnitins311@gmail.com>
2018-03-12 08:22:12 +0530

    Added react-select css style

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {HashRouter, Route, Switch} from 'react-router-dom';
-
+import { Provider } from 'react-redux';
 //PrimeReact Style
 import 'primereact/resources/primereact.min.css';
 import 'primereact/resources/themes/omega/theme.css';
-
+import 'react-select/dist/react-select.css';
 // Styles
 // Import Font Awesome Icons Set
 import 'font-awesome/css/font-awesome.min.css';
@@ -17,10 +17,12 @@ import registerServiceWorker from './registerServiceWorker';
 
 
 ReactDOM.render((
+  
     <HashRouter>
       <Switch>
         <Route path="/" name="Home" component={App}/>
       </Switch>
     </HashRouter>
+  
   ), document.getElementById('root'));
 registerServiceWorker();
