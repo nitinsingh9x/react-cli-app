203b8f7fb9d8e8fcbdb0cf6fb60db12f631ed124
Nitin Singh <singhnitins311@gmail.com>
2018-04-03 21:04:59 +0530

    Removed Toggle From Header Part

--- src/views/shared/header/Header.js
+++ src/views/shared/header/Header.js
@@ -11,35 +11,11 @@ class Header extends Component {
     document.body.classList.toggle('sidebar-hidden');
   }
 
-  sidebarMinimize(e) {
-    e.preventDefault();
-    document.body.classList.toggle('sidebar-minimized');
-  }
-
-  mobileSidebarToggle(e) {
-    e.preventDefault();
-    document.body.classList.toggle('sidebar-mobile-show');
-  }
-
-  asideToggle(e) {
-    e.preventDefault();
-    document.body.classList.toggle('aside-menu-hidden');
-  }
-
   render() {
     return (
       <header className="app-header navbar">
-        <NavbarToggler className="d-lg-none" onClick={this.mobileSidebarToggle}>
-          <span className="navbar-toggler-icon"></span>
-        </NavbarToggler>
         <NavbarBrand href="#"></NavbarBrand>
-        <NavbarToggler className="d-md-down-none mr-auto" onClick={this.sidebarToggle}>
-          <span className="navbar-toggler-icon"></span>
-        </NavbarToggler>
         <div className="app-navbar-right-option">Sign Off</div>
-        {/* <NavbarToggler className="d-md-down-none" onClick={this.asideToggle}>
-          <span className="navbar-toggler-icon"></span>
-        </NavbarToggler> */}
       </header>
     );
   }
